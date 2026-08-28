c7c975463ebe85e55f379fb6163978adb715b93b
Dheeraj Kumar <dheerajdasari88@gmail.com>
2026-08-28 20:25:30 +0530

    Add solution for Add Digits

--- LeetCode/Add-Digits/Solution.java
+++ LeetCode/Add-Digits/Solution.java
@@ -1,24 +1,11 @@
 1class Solution {
 2    public int addDigits(int num) {
-3        int sum =0;
-4        if(num>=0 && num<=9)
-5        {
-6            return num;
-7        }
-8        else
-9        {
-10        while(num>9)
-11        {
-12            sum = 0;
-13            while(num>0)
-14            {
-15                sum+=num%10;
-16                num/=10;
-17            }
-18            num = sum;
-19        }
-20        }
-21        return num;
-22        
-23    }
-24}
+3        if(num>=0 && num<=9)
+4        {
+5            return num;
+6        }
+7        return 1+(num-1)%9;
+8       
+9        
+10    }
+11}
