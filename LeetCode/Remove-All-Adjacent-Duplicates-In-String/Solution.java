a3c7de07bed1d19223b466920df55b9e78b39829
Dheeraj Kumar <dheerajdasari88@gmail.com>
2026-09-16 17:34:09 +0530

    Add solution for Remove All Adjacent Duplicates In String

--- LeetCode/Remove-All-Adjacent-Duplicates-In-String/Solution.java
+++ LeetCode/Remove-All-Adjacent-Duplicates-In-String/Solution.java
@@ -1,37 +1,22 @@
 1class Solution {
 2    public String removeDuplicates(String s) {
-3
-4        Stack<Character> stk = new Stack<>();   
-5        String res = "";
-6
-7        for(char c : s.toCharArray())
-8        {
-9            char ch = c;
-10
-11            if(stk.isEmpty())
-12            {
-13                stk.push(ch);                   
-14            }
-15            else                                
-16            {
-17                if(stk.peek() == ch)
-18                {
-19                    stk.pop();
-20                }
-21                else
-22                {
-23                    stk.push(ch);
-24                }
-25            }
-26        }
-27
-28        while(!stk.isEmpty())
-29        {
-30            res += stk.pop();
-31        }
-32
-33        String sol = new StringBuilder(res).reverse().toString();
-34
-35        return sol;
-36    }
-37}
+3        Stack<Character> stk = new Stack<>();
+4        for(char c: s.toCharArray())
+5        {
+6            if(!stk.isEmpty() && stk.peek()==c)
+7            {
+8                stk.pop();
+9            }
+10            else
+11            {
+12                stk.push(c);
+13            }
+14        }
+15        String res = "";
+16        while(!stk.isEmpty())
+17        {
+18            res = stk.pop()+res; //we can use this as we have only 2 non duplicate characters from the string 
+19        }
+20        return res;
+21    }
+22}
